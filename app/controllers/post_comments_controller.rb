359a83ab9de27c858e042feb525ff9d18d8c74e5
KKe-coder <carumag726@gmail.com>
2020-12-02 07:05:11 +0000

    [Fix] コメントが削除出来ない不具合の修正

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -9,7 +9,8 @@ class PostCommentsController < ApplicationController
   end
 
   def destroy
-    PostComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
+    @postcomment = PostComment.find(params[:book_id])
+    @postcomment.destroy
     redirect_back(fallback_location: root_path)
   end
 
